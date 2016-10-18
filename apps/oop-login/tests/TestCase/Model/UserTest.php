cf6184e257739674ffb3cf32bca0882ac1fb41ad
Raynald Mirville <Greenx72@gmail.com>
2016-10-18 11:45:18 -0400

    initial commit

---
+++ apps/oop-login/tests/TestCase/Model/UserTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace OopLogin\Test\TestCase\Model;
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * setUp method.
+     */
+    public function setUp()
+    {
+    }
+
+    /**
+     * tearDown method.
+     */
+    public function tearDown()
+    {
+    }
+
+    /**
+     * Test constructor.
+     */
+    public function testConstructor()
+    {
+        $this->markTestIncomplete('Not yet implemented.');
+    }
+}
---
+++ apps/oop-login/tests/autoload.php
@@ -0,0 +1,4 @@
+<?php
+
+//require dirname(__DIR__) . '/src/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
