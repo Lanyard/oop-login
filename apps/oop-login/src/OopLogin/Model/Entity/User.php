c3f275d678c026b4a596a976b9ae74d6731c02ae
Raynald Mirville <Greenx72@gmail.com>
2016-11-06 01:15:54 -0500

    Added User getters

--- apps/oop-login/src/OopLogin/Model/Entity/User.php
+++ apps/oop-login/src/OopLogin/Model/Entity/User.php
@@ -3,18 +3,83 @@
 namespace OopLogin\Model\Entity;
 
 /**
- * 
+ * Represents a single user in the database.
  */
 class User
 {
-    private $id;
-    public function __construct()
+    /**
+    * The primary id of the user in the database
+    *
+    * @var integer
+    */
+    protected $id;
+
+    /**
+    * The name of the user
+    *
+    * @var string
+    */
+    protected $username;
+
+    /**
+    * The user's email address
+    *
+    * @var string
+    */
+    protected $email;
+
+    /**
+    * The user's password
+    *
+    * @var string
+    */
+    protected $password;
+
+    /**
+    * Constructor
+    *
+    * @param integer $id The id of the user record.
+    * @param string $username The name of the user
+    * @param string $email The user's email address
+    * @param string $password the user's password
+    */
+    public function __construct($id = -1, $username = '', $email = '', $password = '')
     {
-        $this->id = -1;
+        $this->id = $id;
+        $this->username = $username;
+        $this->email = $email;
+        $this->password = $password;
     }
 
-    public function getID()
+    /**
+    * Get the id of the user record.
+    */
+    public function id()
     {
-        return -1;
+        return $this->id;
+    }
+
+    /**
+    * Get the user's name.
+    */
+    public function username()
+    {
+        return $this->username;
+    }
+
+    /**
+    * Get the user's email address.
+    */
+    public function email()
+    {
+        return $this->email;
+    }
+
+    /**
+    * Get the user's password.
+    */
+    public function password()
+    {
+        return $this->password;
     }
 }
--- apps/oop-login/tests/TestCase/Model/Entity/UserTest.php
+++ apps/oop-login/tests/TestCase/Model/Entity/UserTest.php
@@ -7,11 +7,23 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
+    private $id;
+    private $username;
+    private $email;
+    private $password;
+    private $customUser;
+
     /**
      * setUp method.
      */
     public function setUp()
     {
+        $this->id = 1;
+        $this->username = 'admin';
+        $this->email = 'example@email.com';
+        $this->password = 'dfs';
+
+        $this->customUser = new User($this->id, $this->username, $this->email, $this->password);
     }
 
     /**
@@ -36,6 +48,41 @@ class UserTest extends TestCase
          */
 
         $user = new User();
-        $this->assertEquals($user->getID(), -1);
+        $this->assertEquals($user->id(), -1);
+        $this->assertEquals($user->username(), '');
+        $this->assertEquals($user->email(), '');
+        $this->assertEquals($user->password(), '');
+    }
+
+    /**
+    * Test constructor with custom id
+    */
+    public function testId()
+    {
+        $this->assertEquals(1, $this->customUser->id());
+    }
+
+    /**
+    * Test constructor with custom username
+    */
+    public function testUsername()
+    {
+        $this->assertEquals('admin', $this->customUser->username());
+    }
+
+    /**
+    * Test constructor with custom email
+    */
+    public function testEmail()
+    {
+        $this->assertEquals('example@email.com', $this->customUser->email());
+    }
+
+    /**
+    * Test constructor with custom password
+    */
+    public function testPassword()
+    {
+        $this->assertEquals('dfs', $this->customUser->password());
     }
 }
